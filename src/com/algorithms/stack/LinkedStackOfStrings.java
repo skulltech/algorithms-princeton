f961ff42f0aa365f9b39ee6b17b15784ac00b45b
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-07-01 01:01:51 +0530

    Implement LinkedStackOfStrings

---
+++ src/com/algorithms/stack/LinkedStackOfStrings.java
@@ -0,0 +1,29 @@
+package com.algorithms.stack;
+
+
+public class LinkedStackOfStrings {
+
+    private Node first = null;
+
+    private class Node {
+        String item;
+        Node next;
+    }
+
+    public boolean isEmpty() {
+        return (first == null);
+    }
+
+    public void push(String item) {
+        Node oldfirst = first;
+        Node first = new Node();
+        first.item = item;
+        first.next = oldfirst;
+    }
+
+    public String pop() {
+        String item = first.item;
+        first = first.next;
+        return item;
+    }
+}

bfdb2aa2c73f79280a855dedd5f365931cf5a033
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-07-02 04:47:35 +0530

    Create skeleton code for RandomizedQueue

---
+++ src/RandomizedQueue.java
@@ -0,0 +1,13 @@
+import java.util.Iterator;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+
+    public RandomizedQueue() {}
+    public boolean isEmpty() {}
+    public int size() {}
+    public void enqueue(Item item) {}
+    public Item dequeue() {}
+    public Item sample() {}
+    public Iterator<Item> iterator() {}
+    public static void main(String[] args) {}
+}

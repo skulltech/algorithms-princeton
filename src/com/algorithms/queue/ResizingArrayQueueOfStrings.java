99c7c88b1f7f5c29bae32e538453ee1c5214cf83
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-07-02 00:22:59 +0530

    Skeleton API-only code for Array implementation of queue

---
+++ src/com/algorithms/queue/ResizingArrayQueueOfStrings.java
@@ -0,0 +1,16 @@
+package com.algorithms.queue;
+
+
+public class ResizingArrayQueueOfStrings {
+
+    private String[] queue;
+
+    public ResizingArrayQueueOfStrings() {}
+
+    public boolean isEmpty() {}
+
+    public void push(String item) {}
+
+    public String pop() {}
+
+    private void resize(int capacity) {}

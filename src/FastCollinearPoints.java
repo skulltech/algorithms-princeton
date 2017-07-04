fee8447c1c1e80139cae67d5bdf261f937424200
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-07-04 22:56:27 +0530

    Add FastCollinearPoints. Fix BruteCollinearPoints.

--- src/FastCollinearPoints.java
+++ src/FastCollinearPoints.java
@@ -1,13 +1,11 @@
-public class BruteCollinearPoints {
+import javax.sound.sampled.Line;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class FastCollinearPoints {
 
     private LineSegment[] segments;
-
-    private static boolean collinear(Point[] points) {
-        for (int p = 1; p < points.length - 1; p++) {
-            if (points[0].slopeTo(points[p]) != points[0].slopeTo(points[p+1])) return false;
-        }
-        return true;
-    }
 
     private static boolean hasDuplicate(Point[] points) {
         for (int i = 0; i < points.length; i++) {
@@ -25,21 +23,28 @@ public class BruteCollinearPoints {
         return false;
     }
 
-    public BruteCollinearPoints(Point[] points) {
+    private static boolean collinear(Point[] points) {
+        for (int p = 1; p < points.length - 1; p++) {
+            if (points[0].slopeTo(points[p]) != points[0].slopeTo(points[p+1])) return false;
+        }
+        return true;
+    }
+
+    public FastCollinearPoints(Point[] points) {
         if (points.equals(null) || hasDuplicate(points) || hasNull(points)) throw new IllegalArgumentException();
 
         int N = points.length;
 
-        int counter = 0;
         LineSegment[] buffer = new LineSegment[N];
+        int counter = 0;
         for (int i = 0; i < N; i++) {
-            for (int j = i; j < N; j++) {
-                for (int k = j; k < N; k++) {
-                    for (int l = k; l < N; l++) {
-                        Point[] ps = {points[i], points[j], points[k], points[l]};
-                        if (collinear(ps)) buffer[counter++] = new LineSegment(points[i], points[l]);
-                    }
-                }
+            Comparator<Point> slopeorder = points[i].slopeOrder();
+            Arrays.sort(points, slopeorder);
+
+            for (int j = 0; j < N; j++) {
+                int c = 1;
+                while (collinear(new Point[]{points[i], points[j], points[j + c]})) c++;
+                if (c>2) buffer[counter++] = new LineSegment(points[i], points[j+c-1]);
             }
         }
 

b26a89a79b8a2bdb9f62be9cba2216c1c3159937
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-07-04 19:30:14 +0530

    Create code for Merge-sort

---
+++ src/com/algorithms/sorting/Merge.java
@@ -0,0 +1,47 @@
+package com.algorithms.sorting;
+
+
+public class Merge {
+
+    private static void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
+        if (lo >= hi) return;
+
+        int mid = (lo + hi) / 2;
+        sort(a, aux, lo, mid);
+        sort(a, aux, mid + 1, hi);
+        merge(a, aux, lo, mid, hi);
+    }
+
+    public static void sort(Comparable[] a) {
+        Comparable[] aux = new Comparable[a.length];
+        sort(a, aux, 0, a.length - 1)
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    public static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
+        assert isSorted(a, lo, mid);
+        assert isSorted(a, mid+1, hi);
+
+        for (int i = lo; i < hi; i++) aux[i] = a[i];
+
+        int i = lo, j = mid + 1;
+        for (int k = lo; k < hi; k ++) {
+            if (i > mid) a[k] = aux[j++];
+            else if (j > hi) a[k] = aux[i++];
+            if (less(aux[j], aux[i])) a[k] = aux[j++];
+            else a[k] = aux[i++];
+        }
+
+        assert isSorted(a, lo, hi);
+    }
+
+    private static boolean isSorted(Comparable[] a, int lo, int hi) {
+        for (int i = lo; i < hi; i ++) {
+            if (less(a[i+1], a[i])) return false;
+        }
+        return true;
+    }
+}

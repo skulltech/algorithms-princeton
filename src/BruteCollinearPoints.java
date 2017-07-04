95033d99a5cb0c8b606fbf12306cbc8e0a949e85
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-07-04 22:59:42 +0530

    Add test client to the Classes.

--- src/BruteCollinearPoints.java
+++ src/BruteCollinearPoints.java
@@ -1,3 +1,7 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+
 public class BruteCollinearPoints {
 
     private LineSegment[] segments;
@@ -50,4 +54,34 @@ public class BruteCollinearPoints {
     public int numberOfSegments() { return this.segments.length; }
 
     public LineSegment[] segments() { return this.segments; }
+
+    public static void main(String[] args) {
+
+        // read the n points from a file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        // draw the points
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+
+        // print and draw the line segments
+        BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+        StdDraw.show();
+    }
 }
--- src/FastCollinearPoints.java
+++ src/FastCollinearPoints.java
@@ -1,5 +1,7 @@
-import javax.sound.sampled.Line;
-import java.lang.reflect.Array;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+
 import java.util.Arrays;
 import java.util.Comparator;
 
@@ -55,4 +57,34 @@ public class FastCollinearPoints {
     public int numberOfSegments() { return this.segments.length; }
 
     public LineSegment[] segments() { return this.segments; }
+
+    public static void main(String[] args) {
+
+        // read the n points from a file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        // draw the points
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+
+        // print and draw the line segments
+        FastCollinearPoints collinear = new FastCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+        StdDraw.show();
+    }
 }

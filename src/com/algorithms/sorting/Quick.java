6b017f48cad194738d3b919e07209f53b34d6fff
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-07-06 16:24:47 +0530

    Add 3 way quicksort

--- src/com/algorithms/sorting/Quick.java
+++ src/com/algorithms/sorting/Quick.java
@@ -17,6 +17,23 @@ public class Quick {
         }
     }
 
+    private static void sort3way(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) return;
+        int lt = lo, gt = hi;
+        Comparable v = a[lo];
+        int i = lo;
+
+        while (i <= gt) {
+            int cmp = a[i].compareTo(v);
+            if      (cmp < 0) exch(a, i++, lt++);
+            else if (cmp > 0) exch(a, i, gt--);
+            else              i++;
+        }
+
+        sort(a, lo, lt-1);
+        sort(a, gt+1, hi);
+    }
+
     public void sort(Comparable[] a) {
         StdRandom.shuffle(a);
         sort(a, 0, a.length - 1);

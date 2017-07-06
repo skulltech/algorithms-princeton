3095740bd10e5ec13e3950ca56d2f7bc1f1e9bee
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-07-06 15:19:02 +0530

    Add shuffling to Quicksort

--- src/com/algorithms/sorting/Quick.java
+++ src/com/algorithms/sorting/Quick.java
@@ -1,25 +1,30 @@
 package com.algorithms.sorting;
 
+import edu.princeton.cs.algs4.StdRandom;
+
 
 public class Quick {
 
-    public void sort(Comparable[] a) { sort(a, 0, a.length-1); }
+    public void sort(Comparable[] a) {
+        StdRandom.shuffle(a);
+        sort(a, 0, a.length - 1);
+    }
 
     private static void sort(Comparable[] a, int lo, int hi) {
         if (hi <= lo) return;
         int part = partition(a, lo, hi);
-        sort(a, lo, part-1);
-        sort(a, part+1, hi);
+        sort(a, lo, part - 1);
+        sort(a, part + 1, hi);
     }
 
     private static int partition(Comparable[] a, int lo, int hi) {
-        int i = lo, j = j = hi+1;
+        int i = lo, j = j = hi + 1;
         while (true) {
             while (less(a[++i], a[lo])) if (i == hi) break;
             while (less(a[lo], a[--j])) if (j == lo) break;
 
             if (i >= j) break;
-            exch(a, i , j);
+            exch(a, i, j);
         }
 
         exch(a, lo, j);

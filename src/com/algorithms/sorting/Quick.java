579ca1dc0c26ba4af20c2e9c0f5b619bcd7afd76
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-07-06 15:44:19 +0530

    Add Quick-select

--- src/com/algorithms/sorting/Quick.java
+++ src/com/algorithms/sorting/Quick.java
@@ -5,6 +5,18 @@ import edu.princeton.cs.algs4.StdRandom;
 
 public class Quick {
 
+    public static Comparable select(Comparable[] a, int k) {
+        StdRandom.shuffle(a);
+        int lo = 0, hi = a.length-1;
+        while (true) {
+            int j = partition(a, lo, hi);
+
+            if (j < k)      lo = j+1;
+            else if (j > k) hi = j-1;
+            else            return a[j];
+        }
+    }
+
     public void sort(Comparable[] a) {
         StdRandom.shuffle(a);
         sort(a, 0, a.length - 1);

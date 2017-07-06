49e5d1249b4918ed69ad7d12aea12c5830a05013
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-07-06 13:50:22 +0530

    Create Quick Sort

--- src/com/algorithms/sorting/Quick.java
+++ src/com/algorithms/sorting/Quick.java
@@ -1,4 +1,38 @@
 package com.algorithms.sorting;
 
 
-public class Quick {}
+public class Quick {
+
+    public void sort(Comparable[] a) { sort(a, 0, a.length-1); }
+
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) return;
+        int part = partition(a, lo, hi);
+        sort(a, lo, part-1);
+        sort(a, part+1, hi);
+    }
+
+    private static int partition(Comparable[] a, int lo, int hi) {
+        int i = lo, j = j = hi+1;
+        while (true) {
+            while (less(a[++i], a[lo])) if (i == hi) break;
+            while (less(a[lo], a[--j])) if (j == lo) break;
+
+            if (i >= j) break;
+            exch(a, i , j);
+        }
+
+        exch(a, lo, j);
+        return j;
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+}

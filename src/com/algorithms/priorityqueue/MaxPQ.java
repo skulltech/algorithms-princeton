e8a2cdafdaee75f369f8ff94a4279886fa1a41f8
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-07-07 14:13:23 +0530

    Create base API struct for MaxPQ

---
+++ src/com/algorithms/priorityqueue/MaxPQ.java
@@ -0,0 +1,13 @@
+package com.algorithms.priorityqueue;
+
+
+public class MaxPQ<Key extends Comparable<Key>> {
+
+    public MaxPQ() {}
+    public MaxPQ(Key[] a) {}
+    public void insert(Key v) {}
+    public Key delMax() {}
+    public boolean isEmpty() {}
+    public Key max() {}
+    public int size() {}
+}

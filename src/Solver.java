8a23c7a1fdabb39dac0345b0ffab0936b42bbcc2
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-07-09 21:46:03 +0530

    Almost correct Solver

--- src/Solver.java
+++ src/Solver.java
@@ -2,7 +2,6 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.StdOut;
 
-import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Stack;
 
@@ -16,38 +15,85 @@ public class Solver {
     public Solver(Board initial) {
         if (initial == null) throw new IllegalArgumentException();
 
-        MinPQ<Board> pq = new MinPQ<>(new ManhattanComparator());
-        pq.insert(initial);
+        MinPQ<SearchNode> pq = new MinPQ<>();
+        pq.insert(new SearchNode(initial, 0, null));
 
         Board twin = initial.twin();
-        MinPQ<Board> tpq = new MinPQ<>(new ManhattanComparator());
-        tpq.insert(twin);
+        MinPQ<SearchNode> tpq = new MinPQ<>();
+        tpq.insert(new SearchNode(twin, 0, null));
 
-        Board search = initial, tsearch = twin;
-        while(!search.isGoal() || !tsearch.isGoal()) {
-            search = pq.delMin();
-            tsearch = tpq.delMin();
-            solution.push(search);
-            moves = moves + 1;
+        SearchNode finl = solve(pq, tpq);
 
-            for (Board neighbor: search.neighbors()) {
-                if (!search.equals(neighbor)) pq.insert(neighbor);
+        if (isSolvable) {
+            this.moves = finl.moves;
+            buildSolution(finl);
+        }
+        else {
+            this.moves = -1;
+        }
+    }
+
+    private void buildSolution(SearchNode finl) {
+        while (finl.previous != null) {
+            solution.push(finl.board);
+            finl = finl.previous;
+        }
+        solution.push(finl.board);
+    }
+
+    private SearchNode solve(MinPQ<SearchNode> pq, MinPQ<SearchNode> tpq) {
+        SearchNode node = pq.min(), tnode = pq.min();
+
+        while(!node.board.isGoal() || !tnode.board.isGoal()) {
+            node = pq.delMin();
+            tnode = tpq.delMin();
+
+            for (Board neighbor: node.board.neighbors()) {
+                if (node.previous == null || !node.previous.board.equals(neighbor))
+                    if (neighbor.isGoal()) {
+                        isSolvable = true;
+                        return new SearchNode(neighbor, node.moves+1, node);
+                    }
+                    pq.insert(new SearchNode(neighbor, node.moves+1, node));
             }
-            for (Board tneighbor: tsearch.neighbors()) {
-                if (!tsearch.equals(tneighbor)) tpq.insert(tneighbor);
+            for (Board tneighbor: tnode.board.neighbors()) {
+                if (tnode.previous == null || !tnode.previous.board.equals(tneighbor))
+                    if (tneighbor.isGoal()) {
+                        isSolvable = false;
+                        return new SearchNode(tneighbor, tnode.moves+1, tnode);
+                    }
+                    tpq.insert(new SearchNode(tneighbor, tnode.moves+1, tnode));
             }
         }
 
-        if (search.isGoal()) isSolvable = true;
-        else                 isSolvable = false;
+        if (node.board.isGoal()) {
+            isSolvable = true;
+            return node;
+        }
+        else {
+            isSolvable = false;
+            return tnode;
+        }
     }
 
-    private class ManhattanComparator implements Comparator<Board> {
+    private class SearchNode implements Comparable<SearchNode>{
 
-        public int compare(Board v, Board w) {
-            if      (v.manhattan() < w.manhattan()) return -1;
-            else if (v.manhattan() < w.manhattan()) return +1;
-            else                                    return  0;
+        private Board board;
+        private int moves;
+        private SearchNode previous;
+        public int priority;
+
+        public SearchNode(Board board, int moves, SearchNode previous) {
+            this.board = board;
+            this.moves = moves;
+            this.previous = previous;
+            priority = moves + board.manhattan();
+        }
+
+        public int compareTo(SearchNode that) {
+            if      (this.priority > that.priority) return +1;
+            else if (this.priority < that.priority) return -1;
+            else                                        return  0;
         }
     }
 
@@ -55,7 +101,10 @@ public class Solver {
 
     public int moves() { return this.moves; }
 
-    public Iterable<Board> solution() { return new solution(); }
+    public Iterable<Board> solution() {
+        if (moves != -1) return new solution();
+        else             return null;
+    }
 
     private class solution implements Iterable<Board> {
 

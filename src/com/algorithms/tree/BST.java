e603c858c70f03b4d773d794f5e221f61926ab76
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-09-27 03:28:15 +0530

    Implement put

--- src/com/algorithms/tree/BST.java
+++ src/com/algorithms/tree/BST.java
@@ -14,7 +14,18 @@ public class BST<Key extends Comparable<Key>, Value> {
         }
     }
 
-    public void put(Key key, Value val) {}
+    public void put(Key key, Value val) { this.root = put(root, key, val); }
+
+    private Node put(Node x, Key key, Value val) {
+        if (x==null) return new Node(key, val);
+
+        int cmp = key.compareTo(x.key);
+        if      (cmp > 0) x.right = put(x.right, key, val);
+        else if (cmp < 0) x.left  = put(x.left,  key, val);
+        else                                   x.val = val;
+
+        return x;
+    }
 
     public Value get(Key key) {
         Node x = root;

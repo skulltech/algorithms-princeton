46ce6e626352d845a8b3b151f58837d8200a82e9
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-09-27 02:51:31 +0530

    Create Node class and other skeleton code

--- src/com/algorithms/tree/BST.java
+++ src/com/algorithms/tree/BST.java
@@ -1,4 +1,24 @@
 package com.algorithms.tree;
 
 public class BST<Key extends Comparable<Key>, Value> {
+
+    private Node root;
+
+    private class Node {
+        private Key key;
+        private Value val;
+        private Node left, right;
+        public Node(Key key, Value val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+
+    public void put(Key key, Value val) {}
+
+    public Value get(Key key) {}
+
+    public void delete(Key key) {}
+
+    public Iterable<Key> iterator() {}
 }

e334300b710be92902b3374a37d2abb53d8048f5
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-09-27 02:57:51 +0530

    Implement get

--- src/com/algorithms/tree/BST.java
+++ src/com/algorithms/tree/BST.java
@@ -16,7 +16,18 @@ public class BST<Key extends Comparable<Key>, Value> {
 
     public void put(Key key, Value val) {}
 
-    public Value get(Key key) {}
+    public Value get(Key key) {
+        Node x = root;
+
+        while (x != null) {
+            int cmp = key.compareTo(x.key);
+            if      (cmp < 0) x = x.left;
+            else if (cmp > 0) x = x.right;
+            else              return x.val;
+        }
+
+        return null;
+    }
 
     public void delete(Key key) {}
 

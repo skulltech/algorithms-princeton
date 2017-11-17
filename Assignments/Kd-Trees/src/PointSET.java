930f454781419565adc6712e5b1d1fdc0b8389a0
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-11-18 03:25:29 +0530

    Write the basic implementations

--- Assignments/Kd-Trees/src/PointSET.java
+++ Assignments/Kd-Trees/src/PointSET.java
@@ -1,16 +1,24 @@
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+import java.util.TreeSet;
 
 
 public class PointSET {
-    public PointSET() {}
-    public boolean isEmpty() {}
-    public int size() {}
-    public void insert(Point2D p) {}
-    public boolean contains(Point2D p) {}
+    private TreeSet<Point2D> set = new TreeSet<>();
+
+    public boolean isEmpty() { return set.isEmpty(); }
+    public int size() { return set.size(); }
+    public void insert(Point2D p) { set.add(p); }
+    public boolean contains(Point2D p) { return set.contains(p); }
     public void draw() {}
-    public Iterable<Point2D> range(RectHV rect) {}
-    public Point2D nearest(Point2D p) {}
+
+    public Iterable<Point2D> range(RectHV rect) {
+
+    }
+
+    public Point2D nearest(Point2D p) {
+
+    }
 
     public static void main(String[] args) {}
 }

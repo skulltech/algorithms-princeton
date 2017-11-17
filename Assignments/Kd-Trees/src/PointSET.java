a1b79bd88e7bddbc4df6b89ca1023372cdf407e1
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-11-18 03:52:21 +0530

    Complete PointSET

--- Assignments/Kd-Trees/src/PointSET.java
+++ Assignments/Kd-Trees/src/PointSET.java
@@ -1,5 +1,7 @@
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+
+import java.util.ArrayList;
 import java.util.TreeSet;
 
 
@@ -8,16 +10,33 @@ public class PointSET {
 
     public boolean isEmpty() { return set.isEmpty(); }
     public int size() { return set.size(); }
-    public void insert(Point2D p) { set.add(p); }
+    public void insert(Point2D p) throws IllegalArgumentException {
+        if (p!=null) { set.add(p); }
+        else         { throw new IllegalArgumentException(); }
+    }
     public boolean contains(Point2D p) { return set.contains(p); }
     public void draw() {}
 
-    public Iterable<Point2D> range(RectHV rect) {
+    public Iterable<Point2D> range(RectHV rect) throws IllegalArgumentException {
+        if (rect==null) { throw new IllegalArgumentException(); }
 
+        ArrayList<Point2D> it = new ArrayList<>();
+        for (Point2D p: set) { if (rect.contains(p)) { it.add(p); } }
+        return it;
     }
 
     public Point2D nearest(Point2D p) {
+        Point2D nearest = null;
+        Double distance = Double.POSITIVE_INFINITY;
 
+        for (Point2D current: set) {
+            double dist = current.distanceTo(p);
+            if (dist < distance) {
+                distance = dist;
+                nearest = current;
+            }
+        }
+        return nearest;
     }
 
     public static void main(String[] args) {}

4b0dda2c0feb747587a4a0ae005fb143341505b8
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-11-18 03:21:02 +0530

    Place .gitignore inside all the project dirs

---
+++ Assignments/Kd-Trees/src/PointSET.java
@@ -0,0 +1,16 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+
+public class PointSET {
+    public PointSET() {}
+    public boolean isEmpty() {}
+    public int size() {}
+    public void insert(Point2D p) {}
+    public boolean contains(Point2D p) {}
+    public void draw() {}
+    public Iterable<Point2D> range(RectHV rect) {}
+    public Point2D nearest(Point2D p) {}
+
+    public static void main(String[] args) {}
+}

6d655ec4ecd34662370a734797ee6c676c41df83
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-11-20 15:42:06 +0530

    Complete nearest-neighbor search

--- Assignments/Kd-Trees/src/KdTree.java
+++ Assignments/Kd-Trees/src/KdTree.java
@@ -1,3 +1,4 @@
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 
@@ -37,6 +38,11 @@ public class KdTree {
             if      ((!vertical && rect.ymax() < point.y()) || (vertical && rect.xmax() < point.x())) { return -1; }
             else if ((!vertical && rect.ymin() > point.y()) || (vertical && rect.xmin() > point.x())) { return  1; }
             else                                                                        { return  0; }
+        }
+
+        private double verticalDist(Point2D p) {
+            if (vertical) { return Math.abs(p.y() - point.y()); }
+            else          { return Math.abs(p.x() - point.x()); }
         }
     }
 
@@ -105,15 +111,58 @@ public class KdTree {
         return it;
     }
 
+    private class Champion {
+        private Point2D point;
+        private Double distance;
+
+        private Champion(Point2D point, Double distance) {
+            this.point = point;
+            this.distance = distance;
+        }
+    }
+
 
     public Point2D nearest(Point2D p) {
-        return null;
+        return this.nearest(this.root, new Champion(this.root.point, Double.POSITIVE_INFINITY), p).point;
+    }
+
+    private Champion nearest(Node node, Champion champ, Point2D p) {
+        if (node==null) return champ;
+
+        Double dist = node.point.distanceTo(p);
+        if (dist < champ.distance) {
+            champ.distance = dist;
+            champ.point = node.point;
+        }
+
+        int cmp = node.compare(p);
+        if (cmp < 0) {
+            champ = nearest(node.left, champ, p);
+            if (node.verticalDist(p) < champ.distance) { champ = nearest(node.right, champ, p); }
+        }
+        else if (cmp > 0) {
+            champ = nearest(node.right, champ, p);
+            if (node.verticalDist(p) < champ.distance) { champ = nearest(node.left, champ, p); }
+        }
+        return champ;
     }
 
     public static void main(String[] args) {
         KdTree kdt = new KdTree();
 
-        kdt.insert(new Point2D(1.0, 2.0));
+        String filename = "input10.txt";
+        In in = new In(filename);
+        KdTree kdtree = new KdTree();
+        while (!in.isEmpty()) {
+            double x = in.readDouble();
+            double y = in.readDouble();
+            Point2D p = new Point2D(x, y);
+            kdtree.insert(p);
+        }
+
+        System.out.println(kdtree.nearest(new Point2D(20, 20)));
+
+/*        kdt.insert(new Point2D(1.0, 2.0));
         kdt.insert(new Point2D(15.0, 7.0));
         kdt.insert(new Point2D(7.0, 12.0));
         kdt.insert(new Point2D(4.0, 3.0));
@@ -129,6 +178,6 @@ public class KdTree {
         System.out.println(kdt.contains(new Point2D(-1.0, 20.0)));
 
         RectHV rect = new RectHV(-5, -2, 10, 10);
-        System.out.println(kdt.range(rect));
+        System.out.println(kdt.range(rect));*/
     }
 }
